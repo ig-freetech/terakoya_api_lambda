4dbcd684724b3dc6748c5a2dfe9d6700bcbfa9ca
I.G <i.g.freetech2021@gmail.com>
2023-04-02 00:46:08 +0900

    Delete smtp mail codes

--- functions/utils/mail.py
+++ functions/utils/mail.py
@@ -14,14 +14,14 @@ from botocore.exceptions import ClientError
 ROOT_DIR_PATH = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(ROOT_DIR_PATH)
 
-from config.mail_config import TERAKOYA_GMAIL_ADDRESS, TERAKOYA_GMAIL_PASSWORD
+from config.mail_config import TERAKOYA_GMAIL_ADDRESS
 from config.aws_config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION, SES_TEST_ADDRESS_1, SES_TEST_ADDRESS_2
 
 
 class IMail(metaclass=ABCMeta):
     def __init__(self, mail_from: str) -> None:
         self.msg: MIMEMultipart
-        self.__mail_to = mail_from
+        self.__mail_from = mail_from
 
     @abstractmethod
     def send(self, mail_to: str, subject: str, body: str, mail_cc: str = "", img_fpath: str = "") -> None:
@@ -46,7 +46,7 @@ class IMail(metaclass=ABCMeta):
         self.msg.attach(MIMEText(html_body, "html", policy=SMTPUTF8))
         self.msg["Subject"] = subject
         self.msg["To"] = mail_to
-        self.msg["From"] = self.__mail_to
+        self.msg["From"] = self.__mail_from
         self.msg["Cc"] = mail_cc
 
         if (not (os.path.isfile(img_fpath))):
@@ -55,34 +55,7 @@ class IMail(metaclass=ABCMeta):
             self.msg.attach(MIMEImage(f.read(), name=os.path.basename(img_fpath)))
 
 
-GMAIL_SMTP_HOST_NAME = "smtp.gmail.com"
-GMAIL_SMTP_SSL_DEFAULT_PORT = 465
-
 GMAIL_ACCOUNT = "" if TERAKOYA_GMAIL_ADDRESS is None else TERAKOYA_GMAIL_ADDRESS
-GMAIL_PASSWORD = "" if TERAKOYA_GMAIL_PASSWORD is None else TERAKOYA_GMAIL_PASSWORD
-
-
-class SmtpMail(IMail):
-    def __init__(self, mail_from: str) -> None:
-        super().__init__(mail_from)
-
-    def send(self, mail_to: str, subject: str, body: str, mail_cc: str = "", img_fpath: str = "") -> None:
-        self.write_to_mime(mail_to, subject, body, mail_cc, img_fpath)
-        self.__send_via_smtp()
-
-    def __send_via_smtp(self):
-        print("Try to send an e-mail...")
-        # create a smtp server object to specity the host name (Gmail) and port (SSL)
-        gmail_smtp_server = smtplib.SMTP_SSL(GMAIL_SMTP_HOST_NAME, GMAIL_SMTP_SSL_DEFAULT_PORT)
-        try:
-            gmail_smtp_server.login(GMAIL_ACCOUNT, GMAIL_PASSWORD)
-            gmail_smtp_server.send_message(self.msg)
-        except Exception as e:
-            print(e)  # log an error message
-            sleep(3000)  # wait for timeout(s) of Lambda
-            self.__send_via_smtp()  # recursive
-        print("Email sent!")
-        gmail_smtp_server.quit()
 
 
 class SesMail(IMail):
@@ -125,5 +98,3 @@ if __name__ == "__main__":
     img_fpath = os.path.join(ROOT_DIR_PATH, "assets", "sunshine-map.jpg")
     ses_mail = SesMail(mail_from)
     ses_mail.send(mail_to, subject, body, mail_cc, img_fpath)
-    # smtp_mail = SmtpMail(mail_from)
-    # smtp_mail.send(mail_to, subject, body, mail_cc, img_fpath)
