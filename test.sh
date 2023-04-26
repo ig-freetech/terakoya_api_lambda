13f8220a4b4ca7c15e2c2acc4674f1742cc609b9
I.G <i.g.freetech2021@gmail.com>
2023-04-26 13:27:51 +0000

    Modify deploy.yml

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-if git diff --name-only HEAD^ HEAD | grep "requirements.txt"; then
+if git diff --name-only HEAD^ HEAD -- functions/requirements.txt | grep "requirements.txt"; then
     echo 'requirements.txt has changed'
 else 
     echo 'requirements.txt has not changed'
