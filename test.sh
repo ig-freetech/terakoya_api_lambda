090e352ae9e497c1cca6d1391a0f6154f25ed9e2
I.G <i.g.freetech2021@gmail.com>
2023-04-26 19:31:20 +0000

    Fix serverless.yml for now

--- test.sh
+++
@@ -1,5 +0,0 @@
-if git diff --name-only HEAD^ HEAD -- functions/requirements.txt | grep "requirements.txt"; then
-    echo 'requirements.txt has changed'
-else 
-    echo 'requirements.txt has not changed'
-fi
