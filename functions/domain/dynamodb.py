f83fc212082b0b43acd6bc42b2e54e677859f97c
I.G <i.g.freetech2021@gmail.com>
2023-05-16 08:48:40 +0000

    Add comment and modify table name

--- functions/domain/dynamodb.py
+++ functions/domain/dynamodb.py
@@ -67,7 +67,7 @@ class BookingDynamoDB:
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         region_name=AWS_DEFAULT_REGION
-    ).Table(f"terakoya-booking-renewal-${STAGE}")
+    ).Table(f"terakoya-booking-renewal-{STAGE}")
 
     @classmethod
     def insert_item(cls, item: BookingItem, timestamp: int = int(datetime.now().timestamp())):
