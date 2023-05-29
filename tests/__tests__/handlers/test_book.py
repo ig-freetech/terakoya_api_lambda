50cb79d69bfb6216e9e7a19adf8b0f07ec9c613a
I.G <i.g.freetech2021@gmail.com>
2023-05-29 01:53:54 +0000

    Add agw id (dev) to envs in Github Actions

--- tests/__tests__/handlers/test_book.py
+++ tests/__tests__/handlers/test_book.py
@@ -27,48 +27,48 @@ class TestAPIGateway:
         # Unless specifying Content-Type, the request body is not recognized as JSON and decoded as a string incorrectly in Lambda
         return requests.post(f"{base_url}/book", headers=headers, data=json.dumps({**book_request_body_json, "attendance_date_list": [self.__target_date], "terakoya_type": terakoya_type.value}))
 
-    # def test_place_for_terakoya_type(self):
-    #     # https://www.qbook.jp/column/633.html
-    #     """Black-box testing for /book"""
-    #     for terakoya_type in TERAKOYA_TYPE:
-    #         response = self.__book(terakoya_type)
-    #         print(response)
-    #         response_body = response.json()
-    #         print(response_body)
-    #         assert response.status_code == 200
-    #         assert response_body.get("status_code") == 200
-    #         bk_item = BookingItem(**BookingTable.get_item(self.__target_date, email, terakoya_type))
-    #         assert bk_item.place == TERAKOYA_TYPE_TO_PLACE_MAP[terakoya_type]
+    def test_place_for_terakoya_type(self):
+        # https://www.qbook.jp/column/633.html
+        """Black-box testing for /book"""
+        for terakoya_type in TERAKOYA_TYPE:
+            response = self.__book(terakoya_type)
+            print(response)
+            response_body = response.json()
+            print(response_body)
+            assert response.status_code == 200
+            assert response_body.get("status_code") == 200
+            bk_item = BookingItem(**BookingTable.get_item(self.__target_date, email, terakoya_type))
+            assert bk_item.place == TERAKOYA_TYPE_TO_PLACE_MAP[terakoya_type]
 
 
-# def test_func():
-#     # https://hnavi.co.jp/knowledge/blog/white-box-test/
-#     """White-box testing for /book"""
-#     booking_request = BookingRequest({**book_request_body_json})
+def test_func():
+    # https://hnavi.co.jp/knowledge/blog/white-box-test/
+    """White-box testing for /book"""
+    booking_request = BookingRequest({**book_request_body_json})
 
-#     # List comprehension is better to do the same thing as Array.map(obj => obj.prop) in JavaScript rather than using map(lambda x: x.prop, list)
-#     # https://blog.utgw.net/entry/2017/03/09/154314
-#     date_list = [bk_item.date for bk_item in booking_request.booking_item_list]
-#     # Check whether the same number of booking items are created as the number of attendance dates
-#     assert attendance_date_list[0] in date_list
-#     assert attendance_date_list[1] in date_list
-#     assert attendance_date_list[2] in date_list
+    # List comprehension is better to do the same thing as Array.map(obj => obj.prop) in JavaScript rather than using map(lambda x: x.prop, list)
+    # https://blog.utgw.net/entry/2017/03/09/154314
+    date_list = [bk_item.date for bk_item in booking_request.booking_item_list]
+    # Check whether the same number of booking items are created as the number of attendance dates
+    assert attendance_date_list[0] in date_list
+    assert attendance_date_list[1] in date_list
+    assert attendance_date_list[2] in date_list
 
-#     # Some of props are only tested by equivalence partitioning
-#     # https://e-words.jp/w/%E5%90%8C%E5%80%A4%E5%88%86%E5%89%B2.html
-#     assert booking_request.booking_item_list[0].email == email
-#     assert booking_request.booking_item_list[0].terakoya_type.value == terakoya_type
+    # Some of props are only tested by equivalence partitioning
+    # https://e-words.jp/w/%E5%90%8C%E5%80%A4%E5%88%86%E5%89%B2.html
+    assert booking_request.booking_item_list[0].email == email
+    assert booking_request.booking_item_list[0].terakoya_type.value == terakoya_type
 
-#     # White-box testing for BookingItem's __init__()
-#     assert booking_request.booking_item_list[0].place.value == TERAKOYA_TYPE_TO_PLACE_MAP[TERAKOYA_TYPE(
-#         terakoya_type)].value
-#     assert booking_request.booking_item_list[0].sk == generate_sk(email, TERAKOYA_TYPE(terakoya_type))
+    # White-box testing for BookingItem's __init__()
+    assert booking_request.booking_item_list[0].place.value == TERAKOYA_TYPE_TO_PLACE_MAP[TERAKOYA_TYPE(
+        terakoya_type)].value
+    assert booking_request.booking_item_list[0].sk == generate_sk(email, TERAKOYA_TYPE(terakoya_type))
 
-#     booking_request.book()
+    booking_request.book()
 
-#     booked_item = BookingTable.get_item(date_list[0], email, TERAKOYA_TYPE(terakoya_type))
-#     bk_item = BookingItem(**booked_item)
+    booked_item = BookingTable.get_item(date_list[0], email, TERAKOYA_TYPE(terakoya_type))
+    bk_item = BookingItem(**booked_item)
 
-#     # Test for booking completion
-#     assert bk_item.timestamp == booking_request.booking_item_list[0].timestamp
-#     assert bk_item.uid == booking_request.booking_item_list[0].uid
+    # Test for booking completion
+    assert bk_item.timestamp == booking_request.booking_item_list[0].timestamp
+    assert bk_item.uid == booking_request.booking_item_list[0].uid
