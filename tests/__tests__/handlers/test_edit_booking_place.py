4bfcc7d605910cc97d14a779d631be78b599ffe9
I.G <i.g.freetech2021@gmail.com>
2023-05-29 14:13:40 +0000

    Modify BookingItem to avoid error in test_edit

--- tests/__tests__/handlers/test_edit_booking_place.py
+++ tests/__tests__/handlers/test_edit_booking_place.py
@@ -2,6 +2,8 @@ import os
 import sys
 import json
 import requests
+import logging
+from tenacity import retry, stop_after_attempt, wait_fixed, after_log
 
 ROOT_DIR_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 sys.path.append(ROOT_DIR_PATH)
@@ -17,6 +19,14 @@ class TestAPIGateway:
     def __get_booked_item(self, target_date: str, terakoya_type_value: int) -> BookingItem:
         booked_item = BookingTable.get_item(target_date, email, TERAKOYA_TYPE(terakoya_type_value))
         return BookingItem(**booked_item)
+
+    # Wait for a few seconds for the updated date to be reflected in the database
+    # tenacity enables retrying when the test fails with conditions of timeout secs and interval secs etc...
+    # https://ohke.hateblo.jp/entry/2020/11/21/230000
+    @retry(stop=stop_after_attempt(2), wait=wait_fixed(5), after=after_log(logging.getLogger(__name__), logging.WARNING))
+    def __check_place_updated(self, target_date: str, terakoya_type_value: int, target_place_value: int):
+        bk_item = self.__get_booked_item(target_date, terakoya_type_value)
+        assert bk_item.place.value == target_place_value
 
     def test_place_after_edit(self):
         """Black-box testing for /booking/edit/place"""
@@ -38,5 +48,4 @@ class TestAPIGateway:
         assert response.status_code == 200
         assert response_body.get("status_code") == 200
 
-        bk_item = self.__get_booked_item(target_date, terakoya_type_value)
-        assert bk_item.place.value == target_place_value
+        self.__check_place_updated(target_date, terakoya_type_value, target_place_value)
