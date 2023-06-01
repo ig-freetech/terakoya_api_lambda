8e0cad6736f62f1a9182c5ffb68fe9ca02ab50fb
I.G <i.g.freetech2021@gmail.com>
2023-06-01 13:40:12 +0000

    Modify excluded-dates test

--- tests/__tests__/routers/test_booking.py
+++ tests/__tests__/routers/test_booking.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 
 ROOT_DIR_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(ROOT_DIR_PATH)
@@ -11,7 +12,8 @@ EXCLUDED_DATES_CSV_FOR_TEST_FKEY = f"api/booking/excluded_dates_for_test.csv"
 
 
 def test_update_excluded_dates():
-    dates = ["2023-06-03", "2023-06-10", DT.CURRENT_JST_ISO_8601_ONLY_DATE]
+    dates = [DT.CURRENT_JST_ISO_8601_ONLY_DATE, DT.CURRENT_JST_DATETIME +
+             datetime.timedelta(days=7), DT.CURRENT_JST_DATETIME + datetime.timedelta(days=14)]
     booking.update_excluded_dates(dates, EXCLUDED_DATES_CSV_FOR_TEST_FKEY)
     response = booking.fetch_excluded_dates(EXCLUDED_DATES_CSV_FOR_TEST_FKEY)
     assert response["dates"] == dates
