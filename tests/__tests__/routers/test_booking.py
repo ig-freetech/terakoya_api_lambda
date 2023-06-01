02d9f30d203e25cb4de58a19a421d1ca3270c3ec
I.G <i.g.freetech2021@gmail.com>
2023-06-01 13:48:32 +0000

    Add comment

--- tests/__tests__/routers/test_booking.py
+++ tests/__tests__/routers/test_booking.py
@@ -12,6 +12,8 @@ EXCLUDED_DATES_CSV_FOR_TEST_FKEY = f"api/booking/excluded_dates_for_test.csv"
 
 
 def test_update_excluded_dates():
+    # Add days to datetime object by datetime + timedelta(days={days})
+    # https://pg-chain.com/python-datetime-timedelta
     iso_one_week_later = (DT.CURRENT_JST_DATETIME + datetime.timedelta(days=7)).strftime(ISO_DATE_FORMAT)
     iso_two_week_later = (DT.CURRENT_JST_DATETIME + datetime.timedelta(days=14)).strftime(ISO_DATE_FORMAT)
     dates = [DT.CURRENT_JST_ISO_8601_ONLY_DATE, iso_one_week_later, iso_two_week_later]
