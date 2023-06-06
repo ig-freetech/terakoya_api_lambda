74928f40d34ec3f0353e4bc6a5d95450895df062
I.G <i.g.freetech2021@gmail.com>
2023-06-06 09:42:13 +0000

    Add slack notification via requests with block

---
+++ tests/__tests__/utils/slack_test.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import pytest
+
+ROOT_DIR_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(ROOT_DIR_PATH)
+
+from functions.utils.slack import SlackErrorNotification
+
+
+def test_slack_error_notification():
+    slack_error_notifier = SlackErrorNotification()
+    response = slack_error_notifier.notify("test", "test", {})
+    assert response.status_code == 200
