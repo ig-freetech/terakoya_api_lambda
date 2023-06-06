3a2e52e668941ab0a9470d8c94720d523deb5185
I.G <i.g.freetech2021@gmail.com>
2023-06-06 09:45:39 +0000

    Comment out slack error test

--- tests/__tests__/utils/slack_test.py
+++ tests/__tests__/utils/slack_test.py
@@ -7,7 +7,7 @@ sys.path.append(ROOT_DIR_PATH)
 from functions.utils.slack import SlackErrorNotification
 
 
-def test_slack_error_notification():
-    slack_error_notifier = SlackErrorNotification()
-    response = slack_error_notifier.notify("test", "test", {})
-    assert response.status_code == 200
+# def test_slack_error_notification():
+#     slack_error_notifier = SlackErrorNotification()
+#     response = slack_error_notifier.notify("test", "test", {})
+#     assert response.status_code == 200
