cc7c4359c45a59c3347e118bdb9d37dd2eb8f336
I.G <i.g.freetech2021@gmail.com>
2023-06-20 23:25:44 +0000

    Add comment

--- functions/domain/user.py
+++ functions/domain/user.py
@@ -9,16 +9,18 @@ from conf.env import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGIO
 from models.user import UserItem
 
 __table = boto3.resource(
-        "dynamodb",
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=AWS_DEFAULT_REGION
-    ).Table(f"terakoya-{STAGE}-user")
+    "dynamodb",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_DEFAULT_REGION
+).Table(f"terakoya-{STAGE}-user")
+
 
 def insert_item(item: UserItem):
     # BaseModel must be converted to dict with .dict() method to add an item to DynamoDB table.
     # https://docs.pydantic.dev/latest/usage/exporting_models/#modeldict
     __table.put_item(Item=item.dict())
+
 
 def delete_item(uuid: str, sk: str):
     # https://docs.aws.amazon.com/ja_jp/amazondynamodb/latest/developerguide/GettingStarted.DeleteItem.html
@@ -27,4 +29,4 @@ def delete_item(uuid: str, sk: str):
         # https://confrage.jp/aws-lambdapython3-6%E3%81%8B%E3%82%89dynamodb%E3%81%AE%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92%E5%89%8A%E9%99%A4%E3%81%99%E3%82%8B/
         "uuid": uuid,
         "sk": sk
-        })
+    })
--- functions/models/user.py
+++ functions/models/user.py
@@ -2,8 +2,10 @@ from pydantic import BaseModel
 
 EMPTY_SK = "EMPTY_SK"
 
+
 class UserItem(BaseModel):
     uuid: str
+    # アカウント登録時にStepを2つ用意してStep1でユーザータイプを選択させる？ (sk: student, company, admin, staff)
     # Set a dummy value to sort key by default because Sort key can't be empty string.
     # For now, there's no value to be set as sort key when a user signs up.
     sk: str = EMPTY_SK
