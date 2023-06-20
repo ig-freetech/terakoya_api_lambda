0c51d9013df5c585e79f948a57483eb55e93d6df
I.G <i.g.freetech2021@gmail.com>
2023-06-20 08:16:11 +0000

    Modify post confirmation

--- functions/handlers/auth/post_confirmation.py
+++ functions/handlers/auth/post_confirmation.py
@@ -6,15 +6,24 @@ sys.path.append(FUNCTIONS_DIR_PATH)
 
 from domain.user import insert_item
 from models.user import UserItem
+from utils.slack import SlackErrorNotification
+
+slack_error_notifier = SlackErrorNotification()
 
 # This Lambda function is triggered by PostConfirmation trigger of Cognito User Pool. i.e., when a user clicks the confirmation link in the confirmation email.
 def lambda_handler(event, context):
     print(f"event: {event}")
-    user_name = event['userName']
-    user_email = event['request']['userAttributes']['email']
+    try: 
+        user_name = event['userName']
+        user_email = event['request']['userAttributes']['email']
+        user_item = UserItem(uuid=user_name, email=user_email)
+        print(f"user_item: {user_item.dict()}")
 
-    insert_item(UserItem(uuid=user_name, email=user_email))
+        insert_item(user_item)
 
-    # PostConfirmation trigger must return event
-    # https://docs.aws.amazon.com/ja_jp/cognito/latest/developerguide/user-pool-lambda-post-confirmation.html
-    return event
+        # PostConfirmation trigger must return event
+        # https://docs.aws.amazon.com/ja_jp/cognito/latest/developerguide/user-pool-lambda-post-confirmation.html
+        return event
+    except Exception as e:
+        print(f"Error happend. Error message: {str(e)}")
+        slack_error_notifier.notify(path="post_confirmation", msg=str(e), request_data=event)
--- functions/models/user.py
+++ functions/models/user.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
 
+EMPTY_SK = "EMPTY_SK"
+
 class UserItem(BaseModel):
     uuid: str
-    sk: str = ""
+    # Set a dummy value to sort key by default because Sort key can't be empty string.
+    # For now, there's no value to be set as sort key when a user signs up.
+    sk: str = EMPTY_SK
     email: str
